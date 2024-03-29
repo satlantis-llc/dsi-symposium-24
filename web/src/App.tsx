a06fcb597eee4e07dcc49dc0852892fdb5fd57b2
John Miclat <jmiclat@satlantisllc.com>
2024-03-28 21:14:42 -0400

    Init webapp using React, Typescript, Tailwind, and Shadcn

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ImageForm from '@/components/ImageForm';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>SATLANTIS</h1>
+      <ImageForm />
+    </>
+  );
+}
+
+export default App;
