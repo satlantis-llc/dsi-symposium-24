74ebd25cc5286ad6d7488ac5f40c84da4c846a0a
John Miclat <jmiclat@satlantisllc.com>
2024-03-29 16:38:53 -0400

    fix request

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -28,7 +28,8 @@ const App = () => {
   async function handleClick() {
     setIsLoading(true);
     try {
-      const response = await fetch('http://127.0.0.1:3000/predict', {
+      // const response = await fetch('http://127.0.0.1:3000/predict', {
+      const response = await fetch('http://192.168.128.10:3000/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
