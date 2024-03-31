2280ea0214fe53881eae069fb75e4f1638026282
John Miclat <miclatjohn1@gmail.com>
2024-03-31 03:38:05 -0400

    add more pie chart colors

--- web/src/components/Result.tsx
+++ web/src/components/Result.tsx
@@ -5,7 +5,28 @@ interface ResultProps {
   results: { [key: string]: number };
 }
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const COLORS = [
+  'hsl(0.0, 70%, 50%)',
+  'hsl(18.0, 70%, 50%)',
+  'hsl(36.0, 70%, 50%)',
+  'hsl(54.0, 70%, 50%)',
+  'hsl(72.0, 70%, 50%)',
+  'hsl(90.0, 70%, 50%)',
+  'hsl(108.0, 70%, 50%)',
+  'hsl(126.0, 70%, 50%)',
+  'hsl(144.0, 70%, 50%)',
+  'hsl(162.0, 70%, 50%)',
+  'hsl(180.0, 70%, 50%)',
+  'hsl(198.0, 70%, 50%)',
+  'hsl(216.0, 70%, 50%)',
+  'hsl(234.0, 70%, 50%)',
+  'hsl(252.0, 70%, 50%)',
+  'hsl(270.0, 70%, 50%)',
+  'hsl(288.0, 70%, 50%)',
+  'hsl(306.0, 70%, 50%)',
+  'hsl(324.0, 70%, 50%)',
+  'hsl(342.0, 70%, 50%)',
+];
 
 const Result: React.FC<ResultProps> = ({ results }) => {
   const data = Object.keys(results).map(key => ({
